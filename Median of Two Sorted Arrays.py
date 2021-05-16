f6320f49eee4cd1b1311985b2ceceec10a2a2dc2
greeneyedcoder <mahee.jaiswal@cumminscollege.edu.in>
2021-05-16 20:50:47 +0530

    Update Median of Two Sorted Arrays.py

--- Median of Two Sorted Arrays.py
+++ Median of Two Sorted Arrays.py
@@ -2,9 +2,6 @@ def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         nums = nums1 + nums2
         nums.sort()
        
-
-
-Median = mean of (n/2)th observation and [(n/2)+1]th observation, if n is even.
         if len(nums) % 2 == 0:                            # Median = [(n/2)+1]th observation, if n is even.
             return (nums[int(len(nums)/2 - 1)] + nums[int(len(nums)/2 + 1 - 1)])/2
         else:
